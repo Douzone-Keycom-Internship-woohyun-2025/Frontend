e7f5185cac82eff37562fb119057cfeaef67bf14
neverleaveualong <wmr06244@naver.com>
2025-11-04 16:54:11 +0900

    fix: sidebar 네임밸링 오타  수정

--- src/layouts/ProtectedLayout.tsx
+++ src/layouts/ProtectedLayout.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import Sidebar from "../components/Sidebar/sidebar";
+import Sidebar from "../components/Sidebar/Sidebar";
 import { useAuthStore } from "../store/authStore";
 
 interface ProtectedLayoutProps {
