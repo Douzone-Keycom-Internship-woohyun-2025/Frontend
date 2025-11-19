dfff5c32dbac80fda6aaae20921ebc20d335648b
neverleaveualong <wmr06244@naver.com>
2025-11-19 23:39:07 +0900

    feat: 프리셋 모달 및 삭제 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -8,63 +8,67 @@ import SummaryPage from "./pages/SummaryPage";
 import FavoritesPage from "./pages/FavoritesPage";
 import PresetManagementPage from "./pages/PresetManagementPage";
 import SignupPage from "./pages/SignupPage";
+import { Toaster } from "@/components/ui/toaster";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <HomePage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/patent-search"
-          element={
-            <ProtectedRoute>
-              <PatentSearchPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/summary"
-          element={
-            <ProtectedRoute>
-              <SummaryPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/favorites"
-          element={
-            <ProtectedRoute>
-              <FavoritesPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/preset-management"
-          element={
-            <ProtectedRoute>
-              <PresetManagementPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/help"
-          element={
-            <ProtectedRoute>
-              <HelpPage />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Toaster />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <HomePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/patent-search"
+            element={
+              <ProtectedRoute>
+                <PatentSearchPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/summary"
+            element={
+              <ProtectedRoute>
+                <SummaryPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/favorites"
+            element={
+              <ProtectedRoute>
+                <FavoritesPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/preset-management"
+            element={
+              <ProtectedRoute>
+                <PresetManagementPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/help"
+            element={
+              <ProtectedRoute>
+                <HelpPage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
